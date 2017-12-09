004c1aecfccc2caad0abe765d6435fbcfa001ecb
Oscar West <oscar.west@collectorbank.se>
2017-12-09 15:13:39 +0100

    Add day 2

---
+++ 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int checkSum = 0;
+            var path = AppDomain.CurrentDomain.BaseDirectory + "input.csv";
+            int[][] lines = File.ReadLines(path)
+                .Select(s => Array.ConvertAll(s.Split("\t"), int.Parse))
+                .ToArray();
+
+            foreach (var line in lines)
+            {
+                checkSum += (line.Max() - line.Min());
+            }
+
+            System.Console.WriteLine(checkSum);
+        }
+    }
+}
